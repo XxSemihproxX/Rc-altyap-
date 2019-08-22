a93e18cb38aea649fceb18d7d7f8ca4108ce5fdb
¹¹.⁰⁷.²⁰¹⁹| ѕємιн ♡E∞ #5565 <50422145+XxSemihproxX@users.noreply.github.com>
2019-08-22 16:19:52 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('online oldum!');
+});
+
+bot.on('message',msg => {
+    if(msg.content === 'ping') {msg.reply('Pong');}
+});
+
+
+bot.login('token'); "Token"
